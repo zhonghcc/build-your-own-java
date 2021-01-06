ea7525f41ae6f48e79d7b6abd28b3556c43b7b21
zhonghcc <zhonghcc@gmail.com>
2021-01-06 12:56:48 +0800

    aqsnode

--- basic/src/main/java/byo/util/concurrent/locks/AQSNode.java
+++ basic/src/main/java/byo/util/concurrent/locks/AQSNode.java
@@ -1,4 +1,25 @@
 package byo.util.concurrent.locks;
 
 public class AQSNode {
+    AQSNode prev;
+    AQSNode next;
+    Thread thread;
+
+    /** waitStatus value to indicate thread has cancelled */
+    static final int CANCELLED =  1;
+    /** waitStatus value to indicate successor's thread needs unparking */
+    static final int SIGNAL    = -1;
+    /** waitStatus value to indicate thread is waiting on condition */
+    static final int CONDITION = -2;
+    /**
+     * waitStatus value to indicate the next acquireShared should
+     * unconditionally propagate
+     */
+    static final int PROPAGATE = -3;
+
+    volatile int waitStatus;
+
+    AQSNode(Thread thread){
+        this.thread = thread;
+    }
 }
